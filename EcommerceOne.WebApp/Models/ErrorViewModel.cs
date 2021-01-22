ae4addfc5a5a3c134916020f1e79560580fa1560
Kevin Singh <kevinsingh.developer@gmail.com>
2021-01-22 14:56:01 -0500

    initial commit

---
+++ EcommerceOne.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcommerceOne.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
